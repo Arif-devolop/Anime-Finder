0b6aea1b5016adbbad07567936d779f1137d0cb4
arifbudianto299@gmail.com <arifbudianto299@gmail.com>
2024-09-25 13:49:54 +0800

    verification google meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,11 +8,16 @@ const gabarito = Baloo_2({ subsets: ["latin"] });
 export const metadata = {
   title: "AnimeListWeb",
   description: "website anime indonesia",
+  "google-site-verification": "sgK21-O-reHWCFzHSIfmJx8u5awejouG6zoBKGRHrs8"
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="sgK21-O-reHWCFzHSIfmJx8u5awejouG6zoBKGRHrs8" />
+      </head>
       <body className={`${gabarito.className} bg-color-dark max-w-[1600px] flex flex-col mr-auto ml-auto`}>
         <Navbar/>
         {children}
